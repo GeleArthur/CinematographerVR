2dacaac97f3bead9e5a8a4415ee96c05757960fa
GeleArthur <geleminecrafter@gmail.com>
2025-05-30 00:12:02 +0200

    Started on box raycasting

--- Assets/Scripts/CameraOnRail.cs
+++ Assets/Scripts/CameraOnRail.cs
@@ -25,54 +25,64 @@ public class CameraOnRail : MonoBehaviour
                 Gizmos.color = Color.red;
                 Gizmos.DrawSphere(_railData._nodes[from.Key]._position + (_railData._nodes[to]._position - _railData._nodes[from.Key]._position).normalized * distance, 0.2f);
             }
-            
-            
-            
+        }
+
+        Debug.DrawRay(transform.position, (_target.position - transform.position));
+        if (Physics.Raycast(transform.position, transform.forward, (_target.position - transform.position).magnitude))
+        {
+            Gizmos.color = Color.green;
+            var result = GetClostedPointToTarget(_target.position);
+            Vector3 finalLocation = result.Item1._position + (result.Item2._position - result.Item1._position).normalized * result.Item3;
+            Gizmos.DrawSphere(finalLocation, 0.6f);
+            transform.position = Vector3.Lerp(transform.position, finalLocation, 0.3f);
+        }
+        
+    
+        transform.LookAt(_target);
+    }
+    
+    private (Node, Node, float) GetClostedPointToTarget(Vector3 target)
+    {
+        Node nodeFrom = null;
+        Node nodeTo = null;
+        float offsetOnNode = 0;
+        float distanceResult = float.MaxValue;
+        
+        foreach (Node node in _railData._nodes)
+        {
+            float dist = (target - node._position).sqrMagnitude;
+            if (dist < distanceResult)
+            {
+                distanceResult = dist;
+                nodeFrom = node;
+                nodeTo = node;
+            }
+        }
+        
+        foreach (var from in _railData._connections)
+        {
+            foreach (var to in from.Value._nodes)
+            {
+                float distance = Vector3.Dot(_target.position - _railData._nodes[from.Key]._position, 
+                    (_railData._nodes[to]._position - _railData._nodes[from.Key]._position).normalized);
+                
+                if(distance < 0 || distance > (_railData._nodes[to]._position - _railData._nodes[from.Key]._position).magnitude) continue;
+                
+                Vector3 pointOnLine = _railData._nodes[from.Key]._position + (_railData._nodes[to]._position - _railData._nodes[from.Key]._position).normalized * distance;
+                float dist = (target - pointOnLine).sqrMagnitude;
+                if (dist < distanceResult)
+                {
+                    distanceResult = dist;
+                    offsetOnNode = distance;
+                    nodeFrom = _railData._nodes[from.Key];
+                    nodeTo = _railData._nodes[to];
+                }
+                
+            }
+    
             
         }
         
-        // Gizmos.color = Color.green;
-        // var result = GetClostedPointToTarget(_target.position);
-        // Vector3 finalLocation = result.Item1.Node1.Position + (result.Item1.Node2.Position - result.Item1.Node1.Position).normalized * result.Item2;
-        // Gizmos.DrawSphere(finalLocation, 0.6f);
-    
-        // transform.position = Vector3.Lerp(transform.position, finalLocation, 0.3f);
-        // transform.LookAt(_target);
+        return (nodeFrom, nodeTo, offsetOnNode);
     }
-    
-    // private (Connection, float) GetClostedPointToTarget(Vector3 target)
-    // {
-    //     Node closestNode = null;
-    //     float offsetOnNode = 0;
-    //     float distanceResult = float.MaxValue;
-    //     
-    //     foreach (Node node in RailData._nodes)
-    //     {
-    //         float dist = (target - node._position).sqrMagnitude;
-    //         if (dist < distanceResult)
-    //         {
-    //             distanceResult = dist;
-    //             closestNode = node;
-    //         }
-    //     }
-    //     
-    //     foreach (Connection connection in RailData._connections)
-    //     {
-    //         float distance = Vector3.Dot(_target.position - connection.Node1.Position,
-    //             (connection.Node2.Position - connection.Node1.Position).normalized);
-    //
-    //         if(distance < 0 || distance > (connection.Node2.Position - connection.Node1.Position).magnitude) continue;
-    //         Vector3 pointOnLine = connection.Node1.Position + (connection.Node2.Position - connection.Node1.Position).normalized * distance;
-    //         
-    //         float dist = (target - pointOnLine).sqrMagnitude;
-    //         if (dist < distanceResult)
-    //         {
-    //             distanceResult = dist;
-    //             offsetOnNode = distance;
-    //             closestNode = connection.Node1;
-    //         }
-    //     }
-    //     
-    //     return (_railData.ConnectionLookUp[closestNode!.Index][0], offsetOnNode);
-    // }
 }
